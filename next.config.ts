c711668d6542f53097b4af7863eaa2471ab4523f
TinghuanHsu <TinghuanHsu@uitc.com.tw>
2026-03-27 16:11:13 +0800

    fix(csp): 加入 ecpg-stage domain 到 script-src 和 style-src

--- next.config.ts
+++ next.config.ts
@@ -22,8 +22,8 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://ecpg.ecpay.com.tw https://code.jquery.com https://cdn.jsdelivr.net https://va.vercel-scripts.com",
-              "style-src 'self' 'unsafe-inline' https://ecpg.ecpay.com.tw",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://ecpg.ecpay.com.tw https://ecpg-stage.ecpay.com.tw https://code.jquery.com https://cdn.jsdelivr.net https://va.vercel-scripts.com",
+              "style-src 'self' 'unsafe-inline' https://ecpg.ecpay.com.tw https://ecpg-stage.ecpay.com.tw",
               "connect-src 'self' https://ecpg.ecpay.com.tw https://ecpg-stage.ecpay.com.tw https://ecpayment.ecpay.com.tw https://ecpayment-stage.ecpay.com.tw",
               "frame-src 'self' https://*.ecpay.com.tw",
               "img-src 'self' data: https: blob:",
